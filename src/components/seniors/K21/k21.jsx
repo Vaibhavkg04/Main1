9d05721dca627dbd208e22d2d766fa5d096ee501
Vaibhavkg04 <143902417+Vaibhavkg04@users.noreply.github.com>
2024-01-07 00:17:17 +0530

    Update k21.jsx

--- src/components/seniors/K21/k21.jsx
+++ src/components/seniors/K21/k21.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./K21.css";
+import "./k21.css";
 import Card from "../../card";
 import Card2 from "../../card2";
 let id1;
